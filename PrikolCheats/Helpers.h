b5b8fe2c96e8e926e5affcb346bf84f00edc3697
Барсик Рыжий <ryzijbarsik95@gmail.com>
2024-03-20 22:56:34 +0300

    Перенесена реализация функций в отдельный cpp-файл

--- PrikolCheats/Helpers.h
+++ PrikolCheats/Helpers.h
@@ -1,40 +1,7 @@
 #pragma once
-#include "stdafx.h"
 
-inline cCombatantPtr GetAvatarCombatant()
-{
-	switch (GameNounManager.GetCurrentGameMode())
-	{
-	case GameModeIDs::kGameCreature:
-	case GameModeIDs::kScenarioMode:
-		return GameNounManager.GetAvatar();
-	case GameModeIDs::kGameSpace:
-		return Simulator::GetPlayerUFO();
-	default:
-		return nullptr;
-	}
-}
+cCombatantPtr GetAvatarCombatant();
 
-inline bool TryParseInt32(const char* str, int& result, const ArgScript::FormatParser* parser)
-{
-	try {
-		result = parser->ParseInt(str);
-	}
-	catch (...) {
-		return false;
-	}
-	return true;
-}
+bool TryParseInt32(const char* str, int& result, const ArgScript::FormatParser* parser);
 
-inline uint32_t ParseID(const char* str, const ArgScript::FormatParser* parser)
-{
-	uint32_t res;
-	try {
-		res = parser->ParseUInt(str);
-	}
-	catch (...) {
-		res = id(str);
-	}
-
-	return res;
-}
+uint32_t ParseID(const char* str, const ArgScript::FormatParser* parser);
